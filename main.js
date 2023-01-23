55c135a9902e236ca362b61c2336583c5253e9f0
nukrig <n_gegelashvili@cu.edu.ge>
2023-01-24 02:12:42 +0400

    +

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const data_url='/data.json'
+import {faqs} from "./data.js"
 const faqsElement = document.getElementById('faqs')
 
-let createFaqElement = (element)=>{
+for(let element of faqs){
     const li = document.createElement('li')
     const arrow = document.createElement('img')
     arrow.setAttribute('src', '../images/icon-arrow-down.svg')
@@ -26,11 +26,82 @@ let createFaqElement = (element)=>{
     })
 }
 
-fetch(data_url)
-.then((response)=>{
-    return response.json()
-}).then((data)=>{
-    for(let element of data){
-        createFaqElement(element)
-    }
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const data_url='/data.json'
+// const faqsElement = document.getElementById('faqs')
+
+// let createFaqElement = (element)=>{
+//     const li = document.createElement('li')
+//     const arrow = document.createElement('img')
+//     arrow.setAttribute('src', '../images/icon-arrow-down.svg')
+//     arrow.style.float='right'
+//     li.textContent=element.heading
+//     li.appendChild(arrow)
+//     const p = document.createElement('p')
+//     p.textContent=element.paragraph
+//     li.appendChild(p)
+//     faqsElement.appendChild(li)
+//     li.addEventListener('click',()=>{
+//         if(p.style.display=='none'){
+//             p.style.display='block'
+//             li.style.fontWeight='bold'
+//             p.style.fontWeight='normal'
+//             arrow.style.transform='rotate(180deg)'
+//         }else{
+//             p.style.display='none'
+//             li.style.fontWeight='normal'
+//             arrow.style.transform='rotate(0deg)'
+//         }
+//     })
+// }
+
+// fetch(data_url)
+// .then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     for(let element of data){
+//         createFaqElement(element)
+//     }
+//     })
