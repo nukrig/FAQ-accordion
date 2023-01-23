43210e03326a094d3ca25c5d059829e0fb57e36c
nukrig <n_gegelashvili@cu.edu.ge>
2023-01-24 02:35:01 +0400

    changed arrow

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const faqsElement = document.getElementById('faqs')
 
 for(let element of faqs){
     const li = document.createElement('li')
-    const arrow = document.createElement('img')
-    arrow.setAttribute('src', '../images/icon-arrow-down.svg')
+    const arrow = document.createElement('i')
+    arrow.setAttribute('class', 'fa-solid fa-arrow-down')
     arrow.style.float='right'
+    arrow.style.color='orangered'
     li.textContent=element.heading
     li.appendChild(arrow)
     const p = document.createElement('p')
@@ -13,15 +14,15 @@ for(let element of faqs){
     li.appendChild(p)
     faqsElement.appendChild(li)
     li.addEventListener('click',()=>{
-        if(p.style.display=='none'){
+        if(p.style.display=='block'){
+            p.style.display='none'
+            li.style.fontWeight='normal'
+            arrow.style.transform='rotate(0deg)'
+        }else{
             p.style.display='block'
             li.style.fontWeight='bold'
             p.style.fontWeight='normal'
             arrow.style.transform='rotate(180deg)'
-        }else{
-            p.style.display='none'
-            li.style.fontWeight='normal'
-            arrow.style.transform='rotate(0deg)'
         }
     })
 }
